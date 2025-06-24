4ad051fa01a082042b6c66eecbb4ff99c917c8df
edoardocipriano <edo.cipriano@icloud.com>
2025-06-24 09:21:13 +0200

    Fix circular dependency in DatabaseConfig
    - Remove EntityManagerFactory injection that was causing circular dependency
    - Spring Boot will handle transaction management automatically

--- src/main/java/it/siwbooks/config/DatabaseConfig.java
+++ src/main/java/it/siwbooks/config/DatabaseConfig.java
@@ -1,14 +1,10 @@
 package it.siwbooks.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.orm.jpa.JpaTransactionManager;
-import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.context.annotation.Profile;
 
-import jakarta.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import java.net.URI;
@@ -17,16 +13,6 @@ import java.net.URISyntaxException;
 @Configuration
 @EnableTransactionManagement
 public class DatabaseConfig {
-
-    @Autowired
-    private EntityManagerFactory entityManagerFactory;
-
-    @Bean
-    public PlatformTransactionManager transactionManager() {
-        JpaTransactionManager txManager = new JpaTransactionManager();
-        txManager.setEntityManagerFactory(entityManagerFactory);
-        return txManager;
-    }
 
     @Bean
     @Profile("heroku")
